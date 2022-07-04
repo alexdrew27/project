28cdf910395fe9f4fdefc4ae1f20c1fe426d50dc
alexdrew27 <75916456+alexdrew27@users.noreply.github.com>
2022-07-05 00:54:42 +0300

    Update license_arduino.ino

--- license_arduino/license_arduino.ino
+++ license_arduino/license_arduino.ino
@@ -3,20 +3,21 @@
 #include "DHT.h"
 #include "RTClib.h"
 #include <MCUFRIEND_kbv.h>
-
 DHT dht;
 RTC_DS3231 rtc;
 MCUFRIEND_kbv tft;
-
 bool alarmScreen = false;
-
+int hour, mins = 0;
+int timeSelector = 0;
 void setup () 
 {
   Serial.begin(9600);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(ALARM_BUTTON, INPUT);
-
+  pinMode(SELECT_BUTTON, INPUT);
+  pinMode(INCREMENT_BUTTON, INPUT);
+  pinMode(OK_BUTTON, INPUT);
   digitalWrite(BUZZER_PIN, HIGH);
   
   dht.setup(DHT_PIN);
@@ -26,17 +27,18 @@ void setup ()
     Serial.flush();
     while (1) delay(10);
   }
-
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-
+  rtc.disableAlarm(1);
+  rtc.disableAlarm(2);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
   uint16_t ID = tft.readID();
   tft.begin(ID);
   drawScreen();
 }
-
 void loop ()
 {
   unsigned long currTime = millis();
@@ -53,56 +55,75 @@ void loop ()
   }
   readButtons();
 }
-
 void readButtons() {
   static byte alarm_prev_state;
-  //static byte cancel_prev_state;
-  //static byte min10_prev_state;
-  //static byte min_prev_state;
+  static byte select_prev_state;
+  static byte increment_prev_state;
+  static byte ok_prev_state;
   
   byte alarm_state = digitalRead(ALARM_BUTTON);
-  //byte cancel_state = digitalRead(CANCEL);
-  //byte min10_state = digitalRead(MIN10);
-  //byte min_state = digitalRead(MIN);
-
+  byte select_state = digitalRead(SELECT_BUTTON);
+  byte increment_state = digitalRead(INCREMENT_BUTTON);
+  byte ok_state = digitalRead(OK_BUTTON);
   if(alarm_state && !alarm_prev_state && !alarmScreen) {
     Serial.println("Going to alarm screen");
     alarmScreen = true;
     drawAlarmScreen();
   }
-
   if(alarm_state && !alarm_prev_state && alarmScreen) {
     Serial.println("Going back to main screen");
     alarmScreen = false;
     drawScreen();
   }
+  if(select_state && !select_prev_state && alarmScreen) {
+    // Switch between hour and minutes, also redraw screen.
+    if(timeSelector == 0)
+        timeSelector = 1;
+    if(timeSelector == 1)
+        timeSelector = 0;
+  }
+  if(increment_state && !increment_prev_state && alarmScreen) {
+    // Increment the hours/minutes depending on what's selected, also redraw screen.
+    if(timeSelector == 0) {
+        if(hour + 1 > 23)
+            hour = 0;
+        hour += 1;
+    }
+    else {
+        if(mins + 1 > 59)
+            mins = 0;
+        mins += 1;
+    }
+    drawAlarmScreen();
+  }
+  if(ok_state && !ok_prev_state && alarmScreen) {
+    // Set the alarm and ggo back to the main screen.
+    rtc.setAlarm1(DateTime(2022, 6, 25, hour, mins, 0), DS3231_A1_Hour);
+    alarmScreen = false;
+    drawScreen();
+  }
   
   alarm_prev_state = alarm_state;
-  //min_prev_state = min_state;
-  //sec10_prev_state = sec10_state;
-  //start_prev_state = start_state;
+  select_prev_state = select_state;
+  increment_prev_state = increment_state;
+  ok_prev_state = ok_state;
 }
-
 String getTime() {
   DateTime now = rtc.now();
   char format[] = "hh:mm";
   return now.toString(format);
 }
-
 String getDate() {
   DateTime now = rtc.now();
   char format[] = "DDD, MMM DD YYYY";
   return now.toString(format);
 }
-
 float getTemp() {
   return dht.getTemperature();
 }
-
 int getHumidity() {
   return dht.getHumidity();
 }
-
 void drawScreen() {
   tft.setRotation(1);
   int width = tft.width();
@@ -114,10 +135,9 @@ void drawScreen() {
   drawTemperature();
   drawHumidity();
 }
-
 void drawTemperature() {
   float temp = getTemp();
-  if(temp > 25)
+  if(temp > 30)
     digitalWrite(RED_LED, HIGH);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
@@ -125,7 +145,6 @@ void drawTemperature() {
   tft.print(temp, 1);
   tft.print("C");
 }
-
 void drawHumidity() {
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
@@ -134,21 +153,18 @@ void drawHumidity() {
   tft.print(getHumidity());
   tft.print("%");
 }
-
 void drawDate() {
   tft.setTextSize(3);
   tft.setTextColor(WHITE);
   tft.setCursor(10, 10);
   tft.print(getDate());
 }
-
 void drawTime() {
   tft.setTextSize(5);
   tft.setTextColor(WHITE);
   tft.setCursor(150, 130);
   tft.print(getTime());
 }
-
 void drawAlarmScreen() {
   tft.setRotation(1);
   int width = tft.width();
@@ -158,11 +174,7 @@ void drawAlarmScreen() {
   tft.setTextSize(5);
   tft.setTextColor(WHITE);
   tft.setCursor(150, 130);
-  tft.print("00:00");
-  tft.setTextSize(5);
-  tft.setTextColor(WHITE);
-  tft.setCursor(120, 70);
-  tft.print("Alarm set");
-  Serial.println("Alarm set");
-  delay(5000);
+  tft.print(hour);
+  tft.print(":");
+  tft.print(mins);
 }
